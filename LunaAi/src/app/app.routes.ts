bfec100e185283184d8b981af8e40660f2271b66
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-02-22 13:56:06 +0530

    tailwind and routes setup

--- LunaAi/src/app/app.routes.ts
+++ LunaAi/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './Pages/landing/landing.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { SignupComponent } from './Pages/signup/signup.component';
+import { ChatPageComponent } from './Pages/chat-page/chat-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'chatPage', component: ChatPageComponent },
+    { path: '**', redirectTo: '' }
+];
